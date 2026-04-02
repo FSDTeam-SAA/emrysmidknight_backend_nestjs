7c644779be36795bf7f1e8f30cda4618effe9085
saurav11sarkar <sarkarsaurav11@outlook.com>
2026-04-02 10:33:55 +0600

    feat: initialize session module with controller, service, and mongoose integration

--- src/app/module/session/session.module.ts
+++ src/app/module/session/session.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { SessionService } from './session.service';
 import { SessionController } from './session.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Session, SessionSchema } from './entities/session.entity';
+import { User, UserSchema } from '../user/entities/user.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Session.name, schema: SessionSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
   controllers: [SessionController],
   providers: [SessionService],
 })
--- src/app/module/session/session.service.ts
+++ src/app/module/session/session.service.ts
@@ -1,26 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSessionDto } from './dto/create-session.dto';
 import { UpdateSessionDto } from './dto/update-session.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Session, SessionDocument } from './entities/session.entity';
+import { Model } from 'mongoose';
+import { User, UserDocument } from '../user/entities/user.entity';
 
 @Injectable()
 export class SessionService {
-  create(createSessionDto: CreateSessionDto) {
-    return 'This action adds a new session';
-  }
-
-  findAll() {
-    return `This action returns all session`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} session`;
-  }
-
-  update(id: number, updateSessionDto: UpdateSessionDto) {
-    return `This action updates a #${id} session`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} session`;
-  }
+  constructor(
+    @InjectModel(Session.name)
+    private readonly sessionModel: Model<SessionDocument>,
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+  ) {}
 }
